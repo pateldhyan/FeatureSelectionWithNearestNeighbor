3fdcc861d6128ce46b83a70840dce18458f137f7
Dhyan <dpate148@ucr.edu>
2025-03-07 15:11:33 -0800

    Code setup

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> DataImport(){
+    //Include data import code
+    //ASCII text, 8 place floating numbers
+    //First column indicates class (1 or 2)
+    //Other columns contain features
+}
+
+void FeatureSearch(vector<int> data){
+    //Insert feature search alg. Data import need to be done first
+}
+
+int main(){
+    
+    return 0;
+}
